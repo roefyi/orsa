193802a7c7ca195f4562af02163cc7498a5fa705
roefyi <romansdenson@gmail.com>
2026-01-09 22:37:49 -0600

    Initial Commit

---
+++ orsa/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  orsa
+//
+//  Created by Rome on 1/9/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ orsa/orsaApp.swift
@@ -0,0 +1,17 @@
+//
+//  orsaApp.swift
+//  orsa
+//
+//  Created by Rome on 1/9/26.
+//
+
+import SwiftUI
+
+@main
+struct orsaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
